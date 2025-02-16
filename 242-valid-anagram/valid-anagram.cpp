fafe0caced030a73d3fd6ac538ce3eefb499ea5d
Hassan Majeed <144604910+Rymtall@users.noreply.github.com>
2025-02-16 16:27:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (26.17%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s),end(s));
-        sort(begin(t),end(t));
-        return s==t;
+        vector<int> count(26,0);
+        for(char ch:s){
+            count[ch-'a']++;
+        }
+        for(char ch:t){
+            count[ch-'a']--;
+        }
+
+        bool ans = all_of(begin(count),end(count),[](int ele){
+            return ele == 0;
+        });
+        return ans;
     }
 };
