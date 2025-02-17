e651d2a259a8166768db30b3c45a5c5eeba3031d
Hassan Majeed <144604910+Rymtall@users.noreply.github.com>
2025-02-17 23:06:17 +0530

    Time: 17 ms (5.46%) | Memory: 25.9 MB (12.13%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,65 +1,25 @@
 class Solution {
 public:
-
-    bool isvalid(vector<vector<char>>& board, int sr,int sc){
-       unordered_set<char> ch;
-         for(int i=sr;i<sr+3;i++ ){
-              
-            for(int j = sc;j<sc+3;j++){
-                if(board[i][j] == '.') continue;
-
-                if(ch.find(board[i][j]) != ch.end()){
-                    return false;
-                }
-
-                ch.insert(board[i][j]);
-            }
-         }
-         return true;
-    }
     bool isValidSudoku(vector<vector<char>>& board) {
-       
-       //row check
-        for(int row=0;row<9;row++){
-             unordered_set<char> ch;
-            for(int col =0;col<9;col++){
-                if(board[row][col] == '.') continue;
+        unordered_set<string> st;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]=='.') continue;
+                string row = string(1,board[i][j]) +"_row_"+to_string(i);
+                string col = string(1,board[i][j]) +"_col_"+to_string(j);
+                string box = string(1,board[i][j]) +"_row_"+to_string(i/3)+"_"+to_string(j/3);
 
-                if(ch.find(board[row][col]) != ch.end()){
+                if(st.count(row) || st.count(col) || st.count(box)){
                     return false;
                 }
 
-                ch.insert(board[row][col]);
+                st.insert(row);
+                st.insert(col);
+                st.insert(box);
+
             }
+
         }
-
-        //col check
-        for(int col=0;col<9;col++){
-             unordered_set<char> ch;
-            for(int row =0;row<9;row++){
-                if(board[row][col] == '.') continue;
-
-                if(ch.find(board[row][col]) != ch.end()){
-                    return false;
-                }
-
-                ch.insert(board[row][col]);
-            }
-        }
-
-        //box check
-
-        for(int sr = 0;sr<9;sr+=3){
-            int er =sr+2;
-            for(int sc =0;sc<9;sc+=3){
-                int ec = sc+2;
-                if(!isvalid(board,sr,sc)){
-                    return false;
-                }
-            }
-        }
-       return true;
+        return true;
     }
-
-
 };
